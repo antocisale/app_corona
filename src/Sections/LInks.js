0795e0a7cef7c209102e489ee0eb82d1551d961b
antocisale <antonella.cisale@gmail.com>
2020-03-31 16:41:29 -0300

    responsive css agregado

--- src/Sections/LInks.js
+++ src/Sections/LInks.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useSwitchBoolean from '../Hooks/switchBoolean';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
 
 import '../App.scss';
 
 
 const Links = ()=>{
+    const [menu,toggleMenu] = useSwitchBoolean();
+    
+
+    const showMenu = e=>{
+        toggleMenu();
+    };
+
+    const unwrapMenu = menu ? "nav wrapper shown" : "nav wrapper";
+
     return(
         <div className="header">
             <div>
                 <h1 className="title">Coronavirus</h1>
             </div>
-            <nav>
+            <button className="burger-menu-button"onClick={showMenu}>
+                {
+                    menu ? <FontAwesomeIcon icon={faTimes}/> : <FontAwesomeIcon icon={faBars}/>
+                }
+                
+            </button>
+            <nav className={unwrapMenu}>
                 <Link to="/">
                     Home
                 </Link>
